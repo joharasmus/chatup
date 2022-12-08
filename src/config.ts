9dcf9e714dbe78cd1917e47b1ddbbda1dbecd499
Rasmus Johansson <joha.rasmus@gmail.com>
2022-12-08 16:55:48 +0100

    Set up config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 
 const config = {
-    agora_app_id: process.env.AGORA_APP_ID
+    agora_app_id: process.env.AGORA_APP_ID ?? "ec99ba07a5ef4680bde221a5dd4423bd",
+    agora_app_channel: process.env.AGORA_APP_CHANNEL ?? "itsp",
+    agora_channel_token: process.env.AGORA_CHANNEL_TOKEN ?? "007eJxTYLixqOL+ju2JDWnLZYqNT/7KzJskceeqfclfk7wJjf3BzfYKDKnJlpZJiQbmiaapaSZmFgZJKalGRoaJpikpJiZGxkkpuhyTkhsCGRl2TNnIxMgAgSA+C0NmSXEBAwMA2Q8hCg=="
 }
 
 export default config
